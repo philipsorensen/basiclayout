5a4f8479c9c00b120f7603edb47a8c6ea7bb8949
Philip Sørensen <github@ppsa.dk>
2024-03-22 20:19:47 +0100

    Improved metatitle + status component.

--- src/resources/views/layout/blank.blade.php
+++ src/resources/views/layout/blank.blade.php
@@ -5,10 +5,17 @@
 	<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	@if(View::hasSection('meta_title'))
-		<title>@yield('meta_title') | {{ config('app.name') }}</title>
+	@if (View::hasSection('meta_title'))
+		@if (strlen(View::getSection('meta_title')) <= 82)
+			<title>@yield('meta_title') | {{ config('app.name') }}</title>
+			<meta property="og:title" content="@yield('meta_title') | {{ config('app.name') }}" />
+		@else
+			<title>@yield('meta_title')</title>
+			<meta property="og:title" content="@yield('meta_title')" />
+		@endif
 	@else
-		<title>{{ config('app.name') }}</title>
+		<title>{{ config('app.name') }} | SLOGAN</title>
+		<meta property="og:title" content="{{ config('app.name') }} | SLOGAN" />
 	@endif
 	<meta name="application-name" content="{{ config('app.name') }}" />
 	<meta name="apple-mobile-web-app-title" content="{{ config('app.name') }}" />
@@ -18,10 +25,9 @@
 	<meta name="mobile-web-app-capable" content="yes" />
 	<link rel="canonical" @if (View::hasSection('canonical')) href="@yield('canonical')" @else href="{{ Request::url() }}" @endif>
 	<meta property="og:url" content="{{ Request::url() }}">
-	<meta property="og:title" @if(View::hasSection('meta_title')) content="@yield('meta_title') | {{ config('app.name') }}" @else content="{{ config('app.name') }}" @endif>
 	<meta property="og:description" content="@yield('meta_description')">
 	<meta property="og:image" @if(View::hasSection('og_image')) content="@yield('og_image')" @else content="" @endif>
-	<meta property="og:locale" content="" />
+	<meta property="og:locale" content="{{ config('app.locale') }}" />
 	<meta property="og:type" content="website" />
     <meta name="robots" content="INDEX, FOLLOW">
 	@yield('meta_extra')
